9a7f6eaf8ab27c1b16ae54e502da7fdb33f535fe
shigurenic8 <98692208+shigurenic8@users.noreply.github.com>
2023-04-23 23:23:23 +0900

    Initial commit

---
+++ SharpMap_Test/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace SharpMap_Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mapBox1 = new SharpMap.Forms.MapBox();
+            this.SuspendLayout();
+            // 
+            // mapBox1
+            // 
+            this.mapBox1.ActiveTool = SharpMap.Forms.MapBox.Tools.None;
+            this.mapBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.mapBox1.CustomTool = null;
+            this.mapBox1.FineZoomFactor = 10D;
+            this.mapBox1.Location = new System.Drawing.Point(12, 12);
+            this.mapBox1.MapQueryMode = SharpMap.Forms.MapBox.MapQueryType.LayerByIndex;
+            this.mapBox1.Name = "mapBox1";
+            this.mapBox1.QueryGrowFactor = 5F;
+            this.mapBox1.QueryLayerIndex = 0;
+            this.mapBox1.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(244)))), ((int)(((byte)(244)))), ((int)(((byte)(244)))));
+            this.mapBox1.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(244)))), ((int)(((byte)(244)))));
+            this.mapBox1.ShowProgressUpdate = false;
+            this.mapBox1.Size = new System.Drawing.Size(776, 426);
+            this.mapBox1.TabIndex = 0;
+            this.mapBox1.Text = "mapBox1";
+            this.mapBox1.WheelZoomMagnitude = -2D;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.mapBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private SharpMap.Forms.MapBox mapBox1;
+    }
+}
+
---
+++ SharpMap_Test/Form1.cs
@@ -0,0 +1,82 @@
+﻿using GeoAPI.Geometries;//SharpMap
+using NetTopologySuite.Geometries;//SharpMap
+using SharpMap.Data.Providers;//SharpMap
+using SharpMap.Forms;
+using SharpMap.Layers;//SharpMap
+using SharpMap;//SharpMap
+using System;
+using System.Collections.Generic;//SharpMap
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;//SharpMap
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SharpMap_Test
+{
+    public partial class Form1 : Form
+    {
+        //コンストラクタ
+        public Form1()
+        {
+            InitializeComponent();
+
+            //SharpMap初期化
+            InitSharpMap();
+        }
+
+        private void InitSharpMap()
+        {
+            //======================
+            //SharpMap
+            //参考
+            //http://blog.livedoor.jp/kuro_program/archives/7235669.html
+
+            mapBox1.Map = new Map(new Size(mapBox1.Width, mapBox1.Height));
+            mapBox1.Map.BackColor = Color.LightBlue;
+
+            //レイヤーの作成
+            VectorLayer baseLayer = new VectorLayer("Countries");
+            baseLayer.DataSource = new ShapeFile(@"..\..\ShapeFiles\polbnda_jpn\polbnda_jpn.shp");
+            //baseLayer.DataSource = new ShapeFile(@"..\..\ShapeFiles\ne_10m_coastline\ne_10m_coastline.shp");
+
+            baseLayer.Style.Fill = Brushes.LimeGreen;
+            //baseLayer.Style.Fill = new SolidBrush(Color.LimeGreen);
+            baseLayer.Style.Outline = Pens.Black;
+            baseLayer.Style.EnableOutline = true;
+
+            //マップにレイヤーを追加
+            mapBox1.Map.Layers.Add(baseLayer);
+
+
+            //線と点を書く start -------
+            VectorLayer orgLayer = new VectorLayer("子午線");
+            GeometryFactory gf = new GeometryFactory();
+            List<IGeometry> eomColl = new List<IGeometry>();
+
+            //線を書く
+            Coordinate[] linePos = { new Coordinate(135, 30), new Coordinate(135, 37) };
+            eomColl.Add(gf.CreateLineString(linePos));
+
+            //点を書く
+            eomColl.Add(gf.CreatePoint(new Coordinate(135, 35)));
+
+            GeometryProvider vpro = new GeometryProvider(eomColl);
+            orgLayer.DataSource = vpro;
+
+            mapBox1.Map.Layers.Add(orgLayer);
+            //線と点を書く end -------
+
+
+            //Zoom制限
+            mapBox1.Map.MinimumZoom = 0.1;
+            mapBox1.Map.MaximumZoom = 360.0;
+
+            mapBox1.Map.ZoomToExtents();
+            mapBox1.Refresh();
+            //======================
+        }
+    }
+}

edb49674ac35548168f1f0a2cf9a39bc8227adb8
shigurenic8 <98692208+shigurenic8@users.noreply.github.com>
2023-04-25 01:54:59 +0900

    検討中

--- SharpMap_Test/Form1.Designer.cs
+++ SharpMap_Test/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.mapBox1 = new SharpMap.Forms.MapBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // mapBox1
@@ -37,6 +38,7 @@
             this.mapBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.mapBox1.Cursor = System.Windows.Forms.Cursors.Default;
             this.mapBox1.CustomTool = null;
             this.mapBox1.FineZoomFactor = 10D;
             this.mapBox1.Location = new System.Drawing.Point(12, 12);
@@ -47,26 +49,40 @@
             this.mapBox1.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(244)))), ((int)(((byte)(244)))), ((int)(((byte)(244)))));
             this.mapBox1.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(244)))), ((int)(((byte)(244)))));
             this.mapBox1.ShowProgressUpdate = false;
-            this.mapBox1.Size = new System.Drawing.Size(776, 426);
+            this.mapBox1.Size = new System.Drawing.Size(776, 386);
             this.mapBox1.TabIndex = 0;
             this.mapBox1.Text = "mapBox1";
             this.mapBox1.WheelZoomMagnitude = -2D;
+            this.mapBox1.MouseMove += new SharpMap.Forms.MapBox.MouseEventHandler(this.mapBox1_MouseMove);
+            this.mapBox1.Click += new System.EventHandler(this.mapBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 415);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.mapBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private SharpMap.Forms.MapBox mapBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- SharpMap_Test/Form1.cs
+++ SharpMap_Test/Form1.cs
@@ -13,11 +13,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SharpMap.Drawing;
+using System.Collections.ObjectModel;
 
 namespace SharpMap_Test
 {
     public partial class Form1 : Form
     {
+        //構造体
+        struct StPoint
+        {
+            public int num;
+            public double x;
+            public double y;
+        };
+
+        //クラス変数
+        StPoint g_worldPos = new StPoint();
+
         //コンストラクタ
         public Form1()
         {
@@ -35,7 +48,7 @@ namespace SharpMap_Test
             //http://blog.livedoor.jp/kuro_program/archives/7235669.html
 
             mapBox1.Map = new Map(new Size(mapBox1.Width, mapBox1.Height));
-            mapBox1.Map.BackColor = Color.LightBlue;
+            mapBox1.Map.BackColor = System.Drawing.Color.LightBlue;
 
             //レイヤーの作成
             VectorLayer baseLayer = new VectorLayer("Countries");
@@ -52,7 +65,7 @@ namespace SharpMap_Test
 
 
             //線と点を書く start -------
-            VectorLayer orgLayer = new VectorLayer("子午線");
+            VectorLayer orgLayer = new VectorLayer("symbol");
             GeometryFactory gf = new GeometryFactory();
             List<IGeometry> eomColl = new List<IGeometry>();
 
@@ -77,6 +90,160 @@ namespace SharpMap_Test
             mapBox1.Map.ZoomToExtents();
             mapBox1.Refresh();
             //======================
+        }
+
+        //イベント - 地図上のマウス移動
+        private void mapBox1_MouseMove(Coordinate worldPos, MouseEventArgs imagePos)
+        {
+            UpdateWorldPos(worldPos);
+        }
+
+
+        //イベント - 地図上のクリック
+        private void mapBox1_Click(object sender, EventArgs e)
+        {
+            UpdateSymbolLayer();
+        }
+
+        //世界座標の更新
+        private void UpdateWorldPos(Coordinate worldPos)
+        {
+            g_worldPos.x = worldPos.X;
+            g_worldPos.y = worldPos.Y;
+
+            this.label1.Text = worldPos.ToString();
+        }
+
+        //symbolレイヤーの更新（考え中）
+        private void UpdateSymbolLayer()
+        {
+            //"symbol" レイヤーを取得
+            var symbolLayer = mapBox1.Map.GetLayerByName("symbol") as SharpMap.Layers.VectorLayer;
+
+            //"symbol" レイヤーに描画されている赤色の点を取得
+
+            //点を黄色に変更
+        }
+
+        //削除？
+        private void DeleteLayer()
+        {
+            string layerName = "myLayer"; // 削除するレイヤーの名前
+
+            // レイヤーを取得
+            var layer =  mapBox1.Map.GetLayerByName(layerName) as SharpMap.Layers.VectorLayer;
+
+            // マップコントロールからレイヤーを削除
+            mapBox1.Map.Layers.Remove(layer);
+        }
+
+        //symbolレイヤーの更新（ボツ）
+        private void UpdateSymbolLayer_Botu1()
+        {
+            //symbolレイヤーを取得する
+            VectorLayer symbolLayer = null;
+            string layerName = "symbol";
+
+            foreach (var layer in mapBox1.Map.Layers)
+            {
+                if (layer is VectorLayer vectorLayer && vectorLayer.LayerName == layerName)
+                {
+                    symbolLayer = vectorLayer;
+                    break;
+                }
+            }
+
+            if (symbolLayer != null)
+            {
+                // 交差するジオメトリを指定します（ここでは矩形を指定しています）
+                var geometryFactory = new NetTopologySuite.Geometries.GeometryFactory();
+                var envelope = new GeoAPI.Geometries.Envelope(100, 200, 300, 400);
+                var geometry = geometryFactory.ToGeometry(envelope);
+
+                Console.WriteLine(geometry);
+
+                //List<IGeometry> eomColl = new List<IGeometry>();
+                //IGeometry geometry = eomColl[0]; // 交差するジオメトリを指定する
+
+                SharpMap.Data.FeatureDataSet featureSet = new SharpMap.Data.FeatureDataSet();
+
+                Console.WriteLine(featureSet);
+
+                //symbolLayer.DataSource.GetFeatureTableName(countriesLayer.DataSource.GetGeomType());
+                var featureSetSL = symbolLayer.DataSource as SharpMap.Data.Providers.IProvider;
+
+                Console.WriteLine(featureSetSL);
+
+                featureSet.Tables.Add((SharpMap.Data.FeatureDataTable)featureSetSL);
+                //symbolLayer.DataSource.ExecuteIntersectionQuery(geometry.Envelope, featureSet);
+                //DataTable featureDataTable = featureSet.Tables[0];
+
+
+                /*              
+                 *              SharpMap.Data.FeatureDataSet featureSet = new SharpMap.Data.FeatureDataSet();
+                                var featureSetb = symbolLayer.DataSource as SharpMap.Data.Providers.IProvider;
+                                featureSet.Tables.Add(symbolLayer.DataSource.GetFeatureTableName(symbolLayer.DataSource.GetGeomType()));
+                                countriesLayer.DataSource.ExecuteIntersectionQuery(geometry.Envelope, featureSet);
+                                DataTable featureDataTable = featureSet.Tables[0];
+                */
+
+
+
+                //var featureSet = symbolLayer.DataSource as SharpMap.Data.Providers.IProvider;
+                //var featureDataTable = featureSet.ExecuteIntersectionQuery(geometry.Envelope, (SharpMap.Data.FeatureDataSet)geometry);
+                //var featureDataTable = featureSet.ExecuteIntersectionQuery(geometry.Envelope, (SharpMap.Data.FeatureDataSet)geometry);
+                //foreach (SharpMap.Data.FeatureDataRow row in featureDataTable.Rows)
+
+
+
+                /*                    var features = symbolLayer.DataSource.GetFeaturesIntersection(geometries);
+                                foreach (IFeature feature in features)
+
+
+                                    // symbolレイヤーに描画された赤い点を黄色に変更する
+                                    foreach (var feature in symbolLayer.DataSource.GetGeometriesInView(new GeoAPI.Geometries.Envelope(-90, 90, -180, 180)) )
+                                {
+                                    Geometry geo = new Geometry();
+                                    if (feature.GeometryType is new Geometry().InteriorPoint point )
+                                    {
+                                        point.Symbol = new Symbol(Color.Yellow, 10, new Pen(Color.Yellow));
+                                    }
+                                }
+                */
+
+
+                // MapBoxを再描画する
+                //mapBox1.Refresh();
+            }
+            else
+            {
+                // Countriesレイヤーが見つからなかった場合の処理
+            }
+        }
+
+        //symbolレイヤーの更新（ボツ）
+        private void UpdateSymbolLayer_Botu2()
+        {
+            // 1. "symbol" レイヤーを取得
+            var symbolLayer = mapBox1.Map.GetLayerByName("symbol") as SharpMap.Layers.VectorLayer;
+
+            // 2. "symbol" レイヤーに描画されている赤色の点を取得
+            //var symbolFeatures = symbolLayer.DataSource.GetFeatures().Where(f => f.Geometry is Point && f["color"].ToString() == "red");
+            var symbolFeatures = symbolLayer.DataSource as SharpMap.Data.Providers.IProvider;
+
+            /*            if (!symbolFeatures.Any())
+                        {
+                            // 赤色の点が存在しない場合の処理
+                            return;
+                        }*/
+
+            // 3. 点を黄色に変更
+            foreach (var symbolFeature in symbolFeatures)
+            {
+                symbolFeature.Style.Fill = new SolidBrush(Color.Yellow);
+            }
+            symbolLayer.RenderRequired = true;
+            mapBox1.Refresh();
         }
     }
 }

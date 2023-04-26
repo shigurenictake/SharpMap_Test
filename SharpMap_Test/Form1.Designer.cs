8c6d52ad3e5834cbc9017839c76eca496a27d02c
shigurenic8 <98692208+shigurenic8@users.noreply.github.com>
2023-04-27 01:24:51 +0900

    pointLayer

--- SharpMap_Test/Form1.Designer.cs
+++ SharpMap_Test/Form1.Designer.cs
@@ -30,6 +30,11 @@
         {
             this.mapBox1 = new SharpMap.Forms.MapBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // mapBox1
@@ -38,6 +43,7 @@
             this.mapBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.mapBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.mapBox1.Cursor = System.Windows.Forms.Cursors.Default;
             this.mapBox1.CustomTool = null;
             this.mapBox1.FineZoomFactor = 10D;
@@ -49,7 +55,7 @@
             this.mapBox1.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(244)))), ((int)(((byte)(244)))), ((int)(((byte)(244)))));
             this.mapBox1.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(244)))), ((int)(((byte)(244)))));
             this.mapBox1.ShowProgressUpdate = false;
-            this.mapBox1.Size = new System.Drawing.Size(776, 386);
+            this.mapBox1.Size = new System.Drawing.Size(776, 425);
             this.mapBox1.TabIndex = 0;
             this.mapBox1.Text = "mapBox1";
             this.mapBox1.WheelZoomMagnitude = -2D;
@@ -59,17 +65,70 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 415);
+            this.label1.Location = new System.Drawing.Point(12, 453);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 12);
             this.label1.TabIndex = 1;
             this.label1.Text = "label1";
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 491);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(400, 457);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(400, 491);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 524);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "label3";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(400, 524);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 654);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.mapBox1);
             this.Name = "Form1";
@@ -83,6 +142,11 @@
 
         private SharpMap.Forms.MapBox mapBox1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- SharpMap_Test/Form1.cs
+++ SharpMap_Test/Form1.cs
@@ -21,15 +21,9 @@ namespace SharpMap_Test
     public partial class Form1 : Form
     {
         //構造体
-        struct StPoint
-        {
-            public int num;
-            public double x;
-            public double y;
-        };
-
         //クラス変数
-        StPoint g_worldPos = new StPoint();
+        Coordinate g_worldPos = new Coordinate();
+        System.Drawing.Point g_imagePos = new System.Drawing.Point();
 
         //コンストラクタ
         public Form1()
@@ -37,21 +31,37 @@ namespace SharpMap_Test
             InitializeComponent();
 
             //SharpMap初期化
-            InitSharpMap();
+            this.InitializeMap();
         }
 
-        private void InitSharpMap()
+        //マップ初期化
+        private void InitializeMap()
         {
-            //======================
-            //SharpMap
-            //参考
-            //http://blog.livedoor.jp/kuro_program/archives/7235669.html
+            this.InitializeBaseLayer();
+            this.InitializePointLayer();
+            this.InitializeLineStringLayer();
+            this.InitializeOtherLayer();
 
+            //Zoom制限
+            mapBox1.Map.MinimumZoom = 0.1;
+            mapBox1.Map.MaximumZoom = 360.0;
+
+            //レイヤ全体を表示する(全レイヤの範囲にズームする)
+            mapBox1.Map.ZoomToExtents();
+
+            //mapBoxを再描画
+            mapBox1.Refresh();
+        }
+
+        //基底レイヤ初期化
+        private void InitializeBaseLayer()
+        {
+            //Map生成
             mapBox1.Map = new Map(new Size(mapBox1.Width, mapBox1.Height));
             mapBox1.Map.BackColor = System.Drawing.Color.LightBlue;
 
             //レイヤーの作成
-            VectorLayer baseLayer = new VectorLayer("Countries");
+            VectorLayer baseLayer = new VectorLayer("baseLayer");
             baseLayer.DataSource = new ShapeFile(@"..\..\ShapeFiles\polbnda_jpn\polbnda_jpn.shp");
             //baseLayer.DataSource = new ShapeFile(@"..\..\ShapeFiles\ne_10m_coastline\ne_10m_coastline.shp");
 
@@ -62,7 +72,50 @@ namespace SharpMap_Test
 
             //マップにレイヤーを追加
             mapBox1.Map.Layers.Add(baseLayer);
+        }
 
+        //pointレイヤ初期化
+        private void InitializePointLayer()
+        {
+            //レイヤ生成
+            VectorLayer layer = new VectorLayer("pointLayer");
+            //ジオメトリ生成
+            List<IGeometry> igeoms = new List<IGeometry>();
+            /*
+            //点をジオメトリに追加
+            GeometryFactory gf = new GeometryFactory();
+            igeoms.Add(gf.CreatePoint(new Coordinate(135, 35)));
+            */
+            //ジオメトリをレイヤに反映
+            GeometryProvider gpro = new GeometryProvider(igeoms);
+            layer.DataSource = gpro;
+            //レイヤをmapBoxに追加
+            mapBox1.Map.Layers.Add(layer);
+        }
+
+        //LineStringレイヤ初期化
+        private void InitializeLineStringLayer()
+        {
+            //レイヤ生成
+            VectorLayer layer = new VectorLayer("lineStringLayer");
+            //ジオメトリ生成
+            List<IGeometry> igeoms = new List<IGeometry>();
+            /*
+            //線をジオメトリに追加
+            GeometryFactory gf = new GeometryFactory();
+            Coordinate[] linePos = { new Coordinate(135, 30), new Coordinate(135, 37) };
+            eomColl.Add(gf.CreateLineString(linePos));
+            */
+            //ジオメトリをレイヤに反映
+            GeometryProvider gpro = new GeometryProvider(igeoms);
+            layer.DataSource = gpro;
+            //レイヤをmapBoxに追加
+            mapBox1.Map.Layers.Add(layer);
+        }
+
+        //他のレイヤ初期化
+        private void InitializeOtherLayer()
+        {
             //線と点を書く start -------
             //レイヤの作成
             VectorLayer orgLayer = new VectorLayer("symbolLayer");
@@ -108,50 +161,96 @@ namespace SharpMap_Test
             //レイヤをmapBoxに追加
             mapBox1.Map.Layers.Add(ypLayer);
             //線と点を書く end -------
-
-            //Zoom制限
-            mapBox1.Map.MinimumZoom = 0.1;
-            mapBox1.Map.MaximumZoom = 360.0;
-
-            //全レイヤの範囲にズームする(初期化？)
-            mapBox1.Map.ZoomToExtents();
-
-            //mapBoxを再描画
-            mapBox1.Refresh();
-            //======================
         }
 
         //イベント - 地図上でマウス移動
         private void mapBox1_MouseMove(Coordinate worldPos, MouseEventArgs imagePos)
         {
-            UpdateWorldPos(worldPos);
+            UpdateWorldPos(worldPos);//地理座標系上の座標の更新
+            UpdateImagePos(imagePos);//画面上のピクセル座標の更新
         }
 
         //イベント - 地図上でクリック
         private void mapBox1_Click(object sender, EventArgs e)
         {
-            UpdateSymbolLayer();
+            UpdatePointLayer();//pointLayerレイヤの更新
         }
 
-        //世界座標の更新
+        //イベント - button1クリック
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ViewWholeMap();//全体表示
+        }
+
+        //イベント - button2クリック
+        private void button2_Click(object sender, EventArgs e)
+        {
+            UpdateSymbolLayer(); //symbolレイヤーの更新
+        }
+
+        //イベント - button3クリック
+        private void button3_Click(object sender, EventArgs e)
+        {
+            RemoveLayerOtherThanBase(mapBox1); //ベース以外のレイヤ削除
+        }
+
+        //地理座標系上の座標の更新
         private void UpdateWorldPos(Coordinate worldPos)
         {
-            g_worldPos.x = worldPos.X;
-            g_worldPos.y = worldPos.Y;
+            g_worldPos = worldPos;
 
-            this.label1.Text = worldPos.ToString();
+            //変換テスト
+            this.label1.Text = g_worldPos.ToString() + "\n" +
+                mapBox1.Map.WorldToImage(g_worldPos);
+        }
+
+        //画面上のピクセル座標の更新
+        private void UpdateImagePos(MouseEventArgs imagePos)
+        {
+            g_imagePos.X = imagePos.X;
+            g_imagePos.Y = imagePos.Y;
+
+
+            //変換テスト
+            this.label2.Text = g_imagePos + "\n" +
+                mapBox1.Map.ImageToWorld(g_imagePos);
+        }
+
+        //pointLayerレイヤの更新
+        private void UpdatePointLayer()
+        {
+            //レイヤ取得
+            VectorLayer layer = this.GetVectorLayerByName(mapBox1, "pointLayer");
+            //ジオメトリ取得
+            Collection<IGeometry> igeoms = this.GetIGeometrysAll(layer);
+            //点をジオメトリに追加
+            GeometryFactory gf = new GeometryFactory();
+            igeoms.Add(gf.CreatePoint(g_worldPos));
+            //ジオメトリをレイヤに反映
+            GeometryProvider gpro = new GeometryProvider(igeoms);
+            layer.DataSource = gpro;
+            //レイヤをmapBoxに追加
+            mapBox1.Map.Layers.Add(layer);
+            //mapBoxを再描画
+            mapBox1.Refresh();
+
+            //ラベルにジオメトリを一覧で表示
+            Collection<IGeometry> igeomT = this.GetIGeometrysAll( GetVectorLayerByName(mapBox1, "pointLayer") );
+            string textT = string.Empty;
+            foreach (IGeometry igeom in igeoms) { textT = textT + igeom + "\n"; }
+            this.label3.Text = textT;
         }
 
         //symbolレイヤーの更新
         private void UpdateSymbolLayer()
         {
             //レイヤ取得
-            VectorLayer rlayer = GetVectorLayerByName(mapBox1, "symbolLayer");
-
+            VectorLayer rlayer = this.GetVectorLayerByName(mapBox1, "symbolLayer");
+            //レイヤが存在しない場合は何もしない
+            if (rlayer == null ) { return; }
             //ジオメトリ（地図上に配置した LineString や Point など）を取得
-            Collection<IGeometry> geoms = GetIGeometrys(mapBox1, rlayer);
+            Collection<IGeometry> geoms = this.GetIGeometrysAll(rlayer);
             //foreach (IGeometry geom in geoms) { Console.WriteLine(geom); }
-
             //点を削除(複数該当する場合はindex上で前にいるもの)
             foreach (IGeometry geom in geoms) 
             {
@@ -162,52 +261,13 @@ namespace SharpMap_Test
                     break;
                 }
             }
-
             //レイヤに反映
             GeometryProvider vpro = new GeometryProvider(geoms);
             rlayer.DataSource = vpro;
-
             //レイヤをmapBoxに追加
             mapBox1.Map.Layers.Add(rlayer);
-
-            //全レイヤの範囲にズームする
-            mapBox1.Map.ZoomToExtents();
-
             //mapBoxを再描画
             mapBox1.Refresh();
-        }
-
-        /// <summary>
-        /// ジオメトリ（地図上に配置した LINESTRING や POINT など）を取得
-        /// </summary>
-        /// <param name="mapBox"></param>
-        /// <param name="layer"></param>
-        private Collection<IGeometry> GetIGeometrys(MapBox mapBox, VectorLayer layer)
-        {
-            //指定した領域の特徴を返す Envelope( x1 , x2 , y1, y2)
-            Collection<IGeometry> geoms =
-                layer.DataSource.GetGeometriesInView(
-                    new GeoAPI.Geometries.Envelope(-180, 180, -90, 90) //地図全体(経度-180～180, 緯度-90～90で囲まれる四角形)
-                );
-            return geoms;
-
-            /*
-            //使用例
-            foreach (IGeometry geom in geoms) { Console.WriteLine(geom); }
-            */
-        }
-
-        /// <summary>
-        /// レイヤ削除
-        /// </summary>
-        /// <param name="mapBox"></param>
-        /// <param name="layername"></param>
-        private void RemoveLayer(MapBox mapBox, string layername)
-        {
-            //Layersのindexを初めから検索し最初に該当したレイヤを取得
-            ILayer rlayer = mapBox.Map.Layers.GetLayerByName(layername);
-            //symbolレイヤを削除
-            mapBox1.Map.Layers.Remove(rlayer);
         }
 
         /// <summary>
@@ -219,20 +279,17 @@ namespace SharpMap_Test
         /// <returns></returns>
         private VectorLayer GetVectorLayerByName(MapBox mapBox, string layername)
         {
-            VectorLayer rlayer = new VectorLayer("");
-
-            LayerCollection rlayers = mapBox.Map.Layers;
-            foreach(VectorLayer layer in rlayers)
+            VectorLayer retlayer = null;
+            LayerCollection layers = mapBox.Map.Layers;
+            foreach(VectorLayer layer in layers)
             {
                 if (layer.LayerName == layername)
                 {
-                    rlayer = layer;
+                    retlayer = layer;
                     break;
                 }
             }
-
-            return rlayer;
-
+            return retlayer;
             /*
             //使用例
             //指定した領域()の特徴を返す Envelope( x1 , x2 , y1, y2)
@@ -242,6 +299,72 @@ namespace SharpMap_Test
                 );
             foreach (IGeometry geom in geoms) { Console.WriteLine(geom); }
             */
+        }
+
+        /// <summary>
+        /// ジオメトリ（地図上に配置した LineString や Point など）を取得
+        /// 範囲:地図全体(経度-180～180, 緯度-90～90で囲まれる四角形)
+        /// </summary>
+        /// <param name="layer"></param>
+        private Collection<IGeometry> GetIGeometrysAll(VectorLayer layer)
+        {
+            if (layer == null) { return null; }
+
+            //指定した領域の特徴を返す Envelope( x1 , x2 , y1, y2)
+            //地図全体(経度-180～180, 緯度-90～90で囲まれる四角形)
+            Collection<IGeometry> igeoms =
+                layer.DataSource.GetGeometriesInView(
+                    new GeoAPI.Geometries.Envelope(-180, 180, -90, 90)
+                );
+            return igeoms;
+
+            /*
+            //使用例
+            foreach (IGeometry igeom in igeoms) { Console.WriteLine(geom); }
+            */
+        }
+
+        /// <summary>
+        /// 指定レイヤ削除
+        /// </summary>
+        /// <param name="mapBox"></param>
+        /// <param name="layername"></param>
+        private void RemoveLayer(MapBox mapBox, string layername)
+        {
+            //Layersのindexを初めから検索し最初に該当したレイヤを取得
+            ILayer ilayer = mapBox.Map.Layers.GetLayerByName(layername);
+            //symbolレイヤを削除
+            mapBox1.Map.Layers.Remove(ilayer);
+            //mapBoxを再描画
+            mapBox1.Refresh();
+        }
+
+        /// <summary>
+        /// ベース以外の全レイヤ削除
+        /// <param name="mapBox"></param>
+        /// </summary>
+        private void RemoveLayerOtherThanBase(MapBox mapBox)
+        {
+            //ベース(0番目)以外のレイヤ削除
+            while(mapBox.Map.Layers.Count > 1) {
+                mapBox.Map.Layers.RemoveAt( (mapBox.Map.Layers.Count-1) );
+            }
+            //ベース以外のレイヤ初期化
+            this.InitializePointLayer();
+            this.InitializeLineStringLayer();
+            //mapBoxを再描画
+            mapBox1.Refresh();
+        }
+
+        /// <summary>
+        /// 全体表示
+        /// </summary>
+        private void ViewWholeMap()
+        {
+            //レイヤ全体を表示する(全レイヤの範囲にズームする)
+            mapBox1.Map.ZoomToExtents();
+            //mapBoxを再描画
+            mapBox1.Refresh();
         }
     }
 }
